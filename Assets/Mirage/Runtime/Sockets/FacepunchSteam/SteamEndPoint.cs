88b95bc805b86d91e2d9a2115886b36159062459
Pavel Hodoval <pajah14@gmail.com>
2021-07-26 20:04:59 +0200

    perf: use class for SteamEndPoint to prevent boxing

--- Assets/Mirage/Runtime/Sockets/FacepunchSteam/SteamEndPoint.cs
+++ Assets/Mirage/Runtime/Sockets/FacepunchSteam/SteamEndPoint.cs
@@ -3,8 +3,10 @@ using Mirage.SocketLayer;
 using Steamworks;
 
 namespace Mirage.Sockets.FacepunchSteam {
-    public struct SteamEndPoint : IEndPoint, IEquatable<SteamEndPoint> {
+    public class SteamEndPoint : IEndPoint, IEquatable<SteamEndPoint> {
         public SteamId address;
+
+        public SteamEndPoint() {}
 
         public SteamEndPoint(SteamId address) {
             this.address = address;
